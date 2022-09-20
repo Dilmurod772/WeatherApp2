bad4e2048ac61ee8aae7a7c2634809440edcf30d
Dilmurod <karimovd892@gmail.com>
2022-09-20 15:40:06 +0500

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Karimov Dilmurod on 20/09/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
